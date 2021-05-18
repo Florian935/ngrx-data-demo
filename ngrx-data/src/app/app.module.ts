7e802ca7720ab6c4e5bd84f70687aa5bc95e957f
Florian <florian.martin63000@gmail.com>
2021-05-18 06:09:52 +0200

    Feat: added posts feature

--- ngrx-data/src/app/app.module.ts
+++ ngrx-data/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { AppComponent } from '@app/app.component';
+import { PostsComponent } from '@posts/components/posts.component';
+import { PostsListComponent } from '@posts/components/posts-list/posts-list.component';
+import { EditPostComponent } from '@posts/components/edit-post/edit-post.component';
+import { DetailsPostComponent } from '@posts/components/details-post/details-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    EditPostComponent,
+    DetailsPostComponent,
+    PostsListComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
