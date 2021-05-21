56c71bce50a871daf95437b41b7c281ab3525631
Florian <florian.martin63000@gmail.com>
2021-05-21 07:56:32 +0200

    Feat: WIP Edit Post feature

--- ngrx-data/src/app/pages/posts/components/edit-post/edit-post.component.ts
+++ ngrx-data/src/app/pages/posts/components/edit-post/edit-post.component.ts
@@ -1,15 +1,29 @@
+import { PostService } from './../../services/post.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-edit-post',
-  templateUrl: './edit-post.component.html',
-  styleUrls: ['./edit-post.component.scss']
+    selector: 'app-edit-post',
+    templateUrl: './edit-post.component.html',
+    styleUrls: ['./edit-post.component.scss'],
 })
 export class EditPostComponent implements OnInit {
+    editPostForm!: FormGroup;
+    id?: string;
 
-  constructor() { }
+    constructor(
+        private _formBuilder: FormBuilder,
+        private _postService: PostService,
+        private _route: ActivatedRoute
+    ) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.editPostForm = this._formBuilder.group({
+            title: ['', [Validators.required]],
+            body: ['', [Validators.required]],
+        });
+    }
 
+    onEditPost(): void {}
 }
